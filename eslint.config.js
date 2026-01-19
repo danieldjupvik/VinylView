fe0915927b3252dcde4a38ebda09f8783cf6f995
danieldjupvik <daniel_22@icloud.com>
2026-01-19 01:54:13 +0100

    chore: Update ESLint configuration to include i18next plugin and rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
 import globals from 'globals'
+import i18next from 'eslint-plugin-i18next'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -19,6 +20,9 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite
     ],
+    plugins: {
+      i18next
+    },
     languageOptions: {
       ecmaVersion: 'latest',
       globals: globals.browser,
@@ -26,11 +30,15 @@ export default defineConfig([
         projectService: true,
         tsconfigRootDir: __dirname
       }
+    },
+    rules: {
+      'i18next/no-literal-string': 'error'
     }
   },
   {
     files: ['src/components/ui/**/*.{ts,tsx}'],
     rules: {
+      'i18next/no-literal-string': 'off',
       'react-refresh/only-export-components': 'off'
     }
   }
